f1c36e4aeb5139eece77776d1222c7917946f097
sharthak95 <sharthakmallik@gmail.com>
2024-03-13 01:35:32 +0530

    creates a custom profile service

--- Ecoeden.IdentityServer/IdentityServer/Config.cs
+++ Ecoeden.IdentityServer/IdentityServer/Config.cs
@@ -9,6 +9,13 @@ public static class Config
         {
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
+            new IdentityResources.Email(),
+            new IdentityResource()
+            {
+                Name = "role",
+                DisplayName = "User's role",
+                UserClaims = { "user_role", "permissions" }
+            }
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -24,29 +31,13 @@ public static class Config
             // m2m client credentials flow client
             new Client
             {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
-
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientId = "postman",
+                ClientName = "Postman",
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                RedirectUris = { "https://www.getpostmane.com/oauth2/callback" }, // Not going to be used. nore redirection in postman testing 
                 ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                AllowedScopes = { "scope1" }
-            },
-
-            // interactive client using code flow + pkce
-            new Client
-            {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                AllowedGrantTypes = GrantTypes.Code,
-
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
-            },
+                AllowedScopes = { "openid", "profile", "email", "role" },
+                RequireClientSecret = true
+            }
         };
 }
---
+++ Ecoeden.IdentityServer/IdentityServer/Services/CustomProfileService.cs
@@ -0,0 +1,61 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using IdentityServer.Data;
+using IdentityServer.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Security.Claims;
+
+namespace IdentityServer.Services
+{
+    public class CustomProfileService : IProfileService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public CustomProfileService(UserManager<ApplicationUser> userManager,
+            RoleManager<ApplicationRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var user = await _userManager.GetUserAsync(context.Subject);
+            var roles = await _userManager.GetRolesAsync(user);
+            var permissions = new List<ApplicationPermission>();
+
+            foreach (var roleName in roles)
+            {
+                var role = await _roleManager.Roles.Include(r => r.RolePermissions).ThenInclude(rp => rp.Permission)
+                        .FirstOrDefaultAsync(x => x.Name == roleName);
+                if (role is not null)
+                {
+                    var rolePermissions = role.RolePermissions.Select(rp => rp.Permission).ToList();
+                    permissions.AddRange(rolePermissions);
+                }                
+            }
+
+            var existingClaims = await _userManager.GetClaimsAsync(user);
+
+            var claims = new List<Claim>
+            {
+                new Claim("username", user.UserName),
+                new Claim("roles", JsonConvert.SerializeObject(roles)),
+                new Claim("first name", user.FirstName),
+                new Claim("last name", user.Lastname),
+                new Claim("permissions", JsonConvert.SerializeObject(permissions.Distinct().Select(x => x.Name).ToList()))
+            };
+            context.IssuedClaims.AddRange(claims);
+            context.IssuedClaims.Add(existingClaims.FirstOrDefault(x => x.Type == JwtClaimTypes.Email));
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
