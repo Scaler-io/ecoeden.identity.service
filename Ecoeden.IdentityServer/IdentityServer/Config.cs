b20dfcb94e358e77abf0283d5aa02c87290af245
sharthak1095 <sharthakmallik@gmail.com>
2024-08-10 22:20:18 +0530

    adds new user api client

--- Ecoeden.IdentityServer/IdentityServer/Config.cs
+++ Ecoeden.IdentityServer/IdentityServer/Config.cs
@@ -39,6 +39,17 @@ public static class Config
         {
             new()
             {
+                ClientId = "ecoeden.user.api",
+                ClientName = "User Api",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                RedirectUris = { "https://www.getpostmane.com/oauth2/callback" },
+                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256())},
+                AllowedScopes = { "userapi:read", "userapi:write" },
+                RequireClientSecret = true,
+                AccessTokenType = AccessTokenType.Jwt
+            },
+            new()
+            {
                 ClientId = "ecoeden.catalogue.api",
                 ClientName = "Catalogue Api",
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
